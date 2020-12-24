f331bf44c23ab9c309e901237c3de486a7cc8631
Param Siddharth <contact@paramsid.com>
2020-12-24 17:05:40 +0530

    Environment fix

--- react_frontend/src/services/prediction.jsx
+++ react_frontend/src/services/prediction.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import process from 'process';
 
-const apiUrl = process.env.FLASK_APP_API_URL ?? 'http://localhost:3000';
+const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3000';
 
 const predictSurvival = async (query) => {
 	return await axios.get(apiUrl, {
